5502301e059c78cc07a98f3095f2e08264081c02
DanVasii <82170198+DanVasii@users.noreply.github.com>
2022-07-27 14:23:21 +0300

    test

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,20 @@
 // Put your applicaiton javascript here
+var search_timeout = null;
+jQuery(function(){
+    
+
+    $("#search").on("input",function(elem){
+        if (search_timeout)
+            clearTimeout(search_timeout);
+        search_timeout = setTimeout(()=>{
+            search(elem.target.value);
+        },500)
+    })
+})
+
+
+
+function search(input_value)
+{
+    console.log(input_value);
+}
